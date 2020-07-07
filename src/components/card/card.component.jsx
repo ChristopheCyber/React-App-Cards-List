2cc7a4d09028210b660b792c677782a486125150
Christophe <christophalpha@yahoo.com>
2020-07-07 20:49:07 +0200

    U2_20b section 31; grid with 10 pics,IDs from JSON

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './card.styles.css';
-import img01 from './img/img08.jpg' // relative path to image 
+//import img01 from './img/img01.jpg' // relative path to image 
 
 export const Card = (prop1) => (
     <div className='card-container'> 
         <h1> {prop1.cardProp2.name} </h1>
-        <img src={img01} alt={"pic 01"} />
-        {/*
-        <img title="Image 02" src={ require('./img/min/img02.jpg')} alt={"pic 02"} />
-        <img title="Image 01" src={img01} alt={"pic 01"} />
-        <img src="https://robohash.org/1?set=set3" alt="pic 03"/>
+  
+        <img src={ require(`./img/img${prop1.cardProp2.id}.jpg`)} 
+            alt={"pic 01"} 
+            title={`Image ${prop1.cardProp2.id}`}
+        />
+        {/*     
+        <img src={ require('./img/min/img02.jpg')} alt={"pic 02"} />
+        <img title={"Image 01"} src={img01} alt={"pic 01"} />
+        <img src={`https://robohash.org/${prop1.cardProp2.id}?set=set3`} alt="pic 03"/>
         */}
     </div>
 );
