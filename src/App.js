ad6bd6d5b4b0cf8b67078c064f70ad1d3ec49853
Christophe <christophalpha@yahoo.com>
2020-07-08 19:14:38 +0200

    U2_21a section 32; grid with 10 pics perso and import file json local perso ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component.jsx';
-
+//import jsonData from '../public/data/json-data.json';
 
 class App extends React.Component {
     constructor(){
@@ -10,16 +10,25 @@ class App extends React.Component {
       cards:[]
     }
   }
-  componentDidMount(){
+
+ componentDidMount(){
+  fetch('./data/json-data-users.json')
+  .then ( resp1 => resp1.json() )
+  .then ( users1 => this.setState( {cards : users1} ) )
+}
+  /*--
+    componentDidMount(){
     fetch("https://jsonplaceholder.typicode.com/users")
     .then ( resp1 => resp1.json() )
-    .then ( users => this.setState( {cards : users} ) )
+    .then ( users1 => this.setState( {cards : users1} ) )
   }
+  */
+
   render() {
     return (
       <div className="App">
         <CardList cardsProp1={this.state.cards}>
-          {/* children of CardList component 
+          {/*1 children of CardList component 
           <h2>CardList children here </h2>
           {
             this.state.cards.map( cardElt => 
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,15 +5,20 @@ import './card.styles.css';
 export const Card = (prop1) => (
     <div className='card-container'> 
         <h3> {prop1.cardProp2.name} </h3>
-        <img src={`https://robohash.org/${prop1.cardProp2.id}?set=set4&size=180x180`} alt="pic 03"/>
-        <h4> email: </h4>
-        <span>{prop1.cardProp2.email}</span>
-  {/* 
+        {/* ---my pics--- */}
         <img src={ require(`./img/img${prop1.cardProp2.id}.jpg`)} 
             alt={`Pic ${prop1.cardProp2.id}`} 
             title={`Image ${prop1.cardProp2.id}`}
         />
-            
+        <h4> email: </h4>
+        <span>{prop1.cardProp2.email}</span>
+        {/* 
+        {/*---origin pics---
+        <img src={ require(`./img/img${prop1.cardProp2.id}.jpg`)} 
+            alt={`Pic ${prop1.cardProp2.id}`} 
+            title={`Image ${prop1.cardProp2.id}`}
+        />
+        <img src={`https://robohash.org/${prop1.cardProp2.id}?set=set4&size=180x180`} alt="pic 03"/>
         <img src={ require('./img/min/img02.jpg')} alt={"pic 02"} />
         <img title={"Image 01"} src={img01} alt={"pic 01"} />
         <img src={`https://robohash.org/${prop1.cardProp2.id}?set=set3`} alt="pic 03"/>
