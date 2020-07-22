d4583341c82ae9fd12a964d5bf0e540681318089
Christophe <christophalpha@yahoo.com>
2020-07-22 05:23:43 +0200

    U2_26 / 38; SearchBox reusable functional component+destructuring object method in prop passed

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
 import './search-box.styles.css';
-
-export const SearchBox = ({propPlaceHolder, handleChangeFct }) => (
+/*destructuring object-prop entered*/
+export const SearchBox = ({ propPlaceHolder, handleChangeFct }) => (
   <input
     className='search-box'
     type='search'
-    //destructuring props entered
-    propPlaceHolder1={propPlaceHolder}
+    name="search1"
+    //destructuring obj entered into 2 objs
+    placeHolder={propPlaceHolder}
     onChange={handleChangeFct}
   />
 );
+
+/*no destructuring prop entered, classic affectation
+export const SearchBox = (prop1) => (
+  <input
+    className='search-box'
+    type='search'
+    name="search1"
+    placeHolder={prop1.propPlaceHolder}
+    onChange={prop1.handleChangeFct}
+  />
+);*/
