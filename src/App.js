32e5d4f69e7694bd2c06f2e897a56b1621d32d99
Christophe <christophalpha@yahoo.com>
2020-07-25 01:07:23 +0200

    U2_27 / S40; call Arrow fct method from render method, this. binding; all test comments in

--- src/App.js
+++ src/App.js
@@ -11,34 +11,39 @@ class App extends React.Component {
       searchTxt1: '',
       imgSize1: "img-class"
     };
-    // binding is necessary to make `this` work in the fct1 called in callback method
+    // binding is necessary to make `this` work in a fct1 called as callback method from another method
     //this.fct1 = this.fct1.bind(this);
   }
-
+  //*** Life Cycle method componentDidMount() {}
   componentDidMount() {
-    fetch('./data/json-data-users.json')
+    fetch('./data/json-data-users.json') //fetch("https://jsonplaceholder.typicode.com/users")
       .then(resp1 => resp1.json())
       .then(users1 => this.setState({ cards: users1 }))
   }
-  /*--
-    componentDidMount(){
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then ( resp1 => resp1.json() )
-    .then ( users1 => this.setState( {cards : users1} ) )
-     }
-  */
-  render() {
-    /*destructuring object state, doing same as:*/
-    /* const tabCards = this.state.cards; 
-    const constSearchTxt1 = this.state.searchTxt1; */
-    const { cards: tabCards, searchTxt1: constSearchTxt1, imgSize1: constImgSize1 } = this.state;
-    const filteredCards = tabCards.filter(eltTab =>
-      eltTab.name.toLowerCase().includes(constSearchTxt1.toLowerCase()));
-    console.log('const filteredCards.length=',
-      filteredCards.length);
-    //               
-    //function anime filtered cards
+  //*** method perso created as an Arrow Function
+  methodHandleChange = (evt) => { // (evt) =>{this.setState ( {searchTxt1:evt.target.value} )
+    console.log("Change Event !!!");
+    // setState() 
+    this.setState({ searchTxt1: evt.target.value },
+      () => {
+        console.log("in Event deb searchTxt1=", this.state.searchTxt1,
+          " classAnim=", classAnim);
+        //   fctClassAnim() placed in callback of setState( ,()=>... )   
+        fctClassAnim(this.state.searchTxt1);
+        console.log("in Event fin1 classAnim=", classAnim,
+          '; this.state.imgSize1=', this.state.imgSize1
+        );
+        this.setState({ imgSize1: classAnim },
+          console.log("in Event fin2 classAnim= ", classAnim,
+            '; this.state.imgSize1= ', this.state.imgSize1,
+            '; constImgSize1= ', constImgSize1)
+        );
+      });
+    //*** 
+    //***function fctClassAnim() anime filtered cards */
     var classAnim = "img-class";
+    const tabCards = this.state.cards;
+    const constImgSize1 = this.state.imgSize1;
     function fctClassAnim(eltSearch) {
       console.log('in fctClassAnim()=> eltSearch=', eltSearch);
       /*constSearchTxt1 = this.state.searchTxt1;*/
@@ -64,6 +69,20 @@ class App extends React.Component {
         document.querySelector('.img-class'));
 
     }
+    //
+  }
+  //*** Life Cycle method render() {}
+  render() {
+    /*destructuring object state, doing same as:*/
+    /* const tabCards = this.state.cards; 
+    const constSearchTxt1 = this.state.searchTxt1; */
+    const { cards: tabCards, searchTxt1: constSearchTxt1, imgSize1: constImgSize1 } = this.state;
+    const filteredCards = tabCards.filter(eltTab =>
+      eltTab.name.toLowerCase().includes(constSearchTxt1.toLowerCase()));
+    console.log('const filteredCards.length=',
+      filteredCards.length);
+    //               
+
     //counting filtered cards + according text
     var nbrCards;
     if (filteredCards.length > 1) {
@@ -88,38 +107,14 @@ class App extends React.Component {
     return (
       <div className="App img-class">
         <label>
-          <i className="bigLow js-grid">
-            Search in cards titles -&gt;
-          </i>
+          <i className="bigLow js-grid">Search in cards titles -&gt;</i>
         </label>
         <SearchBox
           propPlaceHolder="Type text for Searching"
-          handleChangeFct={  /* (evt) =>{
-                       this.setState ( {searchTxt1:evt.target.value} )*/
-            (evt) => {
-              console.log("Change Event !!!");
-              //fctClassAnim() placed in callback of setState( ,()=>... )
-              this.setState({ searchTxt1: evt.target.value },
-                () => {
-                  console.log("in Event deb searchTxt1=", this.state.searchTxt1,
-                    " classAnim=", classAnim);
-                  fctClassAnim(this.state.searchTxt1);
-                  console.log("in Event fin1 classAnim=", classAnim,
-                    '; this.state.imgSize1=', this.state.imgSize1
-                  );
-                  this.setState({ imgSize1: classAnim },
-                    console.log("in Event fin2 classAnim= ", classAnim,
-                      '; this.state.imgSize1= ', this.state.imgSize1,
-                      '; constImgSize1= ', constImgSize1)
-                  );
-                });
-            }
-          }
+          handleChangeFct={this.methodHandleChange}
         />
         <label>
-          <i className={"bigLow " + filterBigLowNumber}>
-            &nbsp; {nbrCards}
-          </i>
+          <i className={"bigLow " + filterBigLowNumber}>&nbsp; {nbrCards}</i>
         </label>
 
         {/*<CardList cardsProp1={this.state.cards} />*/}
@@ -133,7 +128,7 @@ class App extends React.Component {
         </CardList> 
           */}
       </div>
-    )
+    );
   }
 }
 export default App;
